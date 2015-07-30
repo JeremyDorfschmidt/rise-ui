99afeaaa616d8291edd5fcc0a3ef12b70eb5677b
Aleks Iraguha <aleks.iraguha@gmail.com>
2015-07-30 13:36:42 -0300

    Toggle mobile menu

--- src/js/components/main-nav.js
+++ src/js/components/main-nav.js
@@ -2,17 +2,19 @@ var React = require('react');
 import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
 import riseLogo from '../../images/rise-logo-black.png';
 import SearchBar from './search-bar.js';
+import $ from 'jquery';
 
 var MainNav = React.createClass({
   render:function(){
     return (
       <div className="ri-app-nav-bar">
 
+        <i className="fa fa-bars mobile-menu-btn" onClick={this.toggleMobileMenu}></i>
         <span className="ri-ise-logo">
           <Link to="home"><img src={riseLogo}/></Link>
         </span>
 
-        <div className="ri-nav-menu">
+        <div ref="menu" className="ri-nav-menu">
           <Link to="home">Home</Link>
           <Link to="photos">Photos</Link>
           <Link to="products">Products</Link>
@@ -23,6 +25,10 @@ var MainNav = React.createClass({
 
       </div>
     );
+  },
+
+  toggleMobileMenu:function(){
+    $(this.refs.menu.getDOMNode()).toggleClass('mobile-menu-visible');
   }
 });
 
