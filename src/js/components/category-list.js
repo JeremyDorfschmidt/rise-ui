3ebe76451e430023f98a8b22c1218fe3df6a8c74
Aleks Iraguha <aleks.iraguha@gmail.com>
2015-07-30 14:02:23 -0300

    Make category page responsive

--- src/js/components/category-list.js
+++ src/js/components/category-list.js
@@ -19,15 +19,44 @@ var CategoryListItem = React.createClass({
 var CategoryList = React.createClass({
   render:function(){
     return (
-      <div className="ri-category-list">
-        <CategoryListItem name="interior" title="Interior Finishes"/>
-        <CategoryListItem name="exterior" title="Exterior Finishes"/>
-        <CategoryListItem name="construction" title="Construction"/>
-        <CategoryListItem name="windows-doors" title="Windows and Doors"/>
-        <CategoryListItem name="landscaping" title="Landscaping"/>
-        <CategoryListItem name="insulation" title="Insulation"/>
-        <CategoryListItem name="electrical" title="Electrical"/>
-        <CategoryListItem name="plumbing" title="Plumbing"/>
+      <div className="ri-category-list container">
+
+        <div className="row">
+          <div className=" col-md-3 col-sm-6">
+            <CategoryListItem name="interior" title="Interior Finishes"/>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <CategoryListItem name="exterior" title="Exterior Finishes"/>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <CategoryListItem name="construction" title="Construction"/>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <CategoryListItem name="windows-doors" title="Windows and Doors"/>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-3 col-sm-6">
+            <CategoryListItem name="landscaping" title="Landscaping"/>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <CategoryListItem name="insulation" title="Insulation"/>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <CategoryListItem name="electrical" title="Electrical"/>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <CategoryListItem name="plumbing" title="Plumbing"/>
+          </div>
+        </div>
+
       </div>
     );
   }
